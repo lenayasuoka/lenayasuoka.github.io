876c53790242f3a4be2db8c1395b8341b25bd27b
Léna Yumie Trullier <lena.trullier@icloud.com>
2024-09-29 21:20:02 +0200

    let's go!

---
+++ navigation.php
@@ -0,0 +1,16 @@
+<?php
+//Open your function
+function footer() {
+//Note php closing tag below
+?>
+
+<div class="footer">
+      <img src="images/radis002.gif" id="gif" alt="radis-gif"/>
+      <br>
+      Last updated September 28, 2024
+</div>
+
+<?php
+//Close your function
+}
+?>

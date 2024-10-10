25ad7e47621aa18f97069de90f41bb807e818db8
Léna Yumie Trullier <lena.trullier@icloud.com>
2024-10-10 22:34:09 +0200

    refresh

---
+++ script1.js
@@ -0,0 +1,68 @@
+var myVideo1 = document.getElementById("video1"); 
+
+ /* function playPause1() { 
+    if (myVideo1.paused) 
+      myVideo1.play(); 
+    else 
+      myVideo1.pause(); 
+  } */
+  function makeBig1() { 
+    myVideo1.width = 1160;
+    myVideo1.height = 651; 
+  } 
+  function makeSmall1() {
+    myVideo1.width = 530; 
+    myVideo1.height = 300; 
+  }
+  
+var myVideo2 = document.getElementById("video2"); 
+
+ /* function playPause2() { 
+    if (myVideo2.paused) 
+      myVideo2.play(); 
+    else 
+      myVideo2.pause(); 
+  } */
+  function makeBig2() { 
+    myVideo2.width = 1160; 
+    myVideo2.height = 651; 
+  } 
+  function makeSmall2() {
+    myVideo2.width = 530; 
+    myVideo2.height = 300; 
+  }
+
+var myVideo3 = document.getElementById("video3"); 
+
+ /* function playPause3() { 
+    if (myVideo3.paused) 
+      myVideo3.play(); 
+    else 
+      myVideo3.pause(); 
+  } */
+  function makeBig3() { 
+    myVideo3.width = 1160; 
+    myVideo3.height = 651; 
+  } 
+  function makeSmall3() {
+    myVideo3.width = 530; 
+    myVideo3.height = 300; 
+  }
+
+ var myVideo4 = document.getElementById("video4"); 
+
+  /* function playPause4() { 
+     if (myVideo4.paused) 
+       myVideo4.play(); 
+     else 
+       myVideo4.pause(); 
+   } */
+   function makeBig4() { 
+     myVideo4.width = 1160; 
+     myVideo4.height = 651; 
+   } 
+   function makeSmall3() {
+     myVideo4.width = 530; 
+     myVideo4.height = 300; 
+   }
+  
---
+++ scriptappear.js
@@ -0,0 +1,67 @@
+/*
+	methods called by index.html
+*/
+
+// list of objects made visible, per level
+var remember = new Array();
+
+/*
+	close everything below level 'level' if needed,
+	show item 'name'
+	and remember it as item shown for level 'level'
+*/
+function show(name,level)
+{
+	// if not the first call, then there's something in remember
+	if( remember[level] )
+	{
+		// if what is supposed to be shown is already visible, don't do anything
+		if( remember[level] == name )
+			return;
+		
+		// if not, then it means everything below level should change, so close all
+		closeAll(level);
+   }
+
+   // if name is provided, make it visible
+   if( name )
+   {
+		var x = getObj(name);
+		x.visibility = 'visible';
+   }
+   // remember it
+   remember[level] = name;
+}
+
+/*
+	close everything below level 'level'
+*/
+function closeAll(level)
+{
+	for( i = remember.length - 1; i >= level; i-- )
+	{
+		if( remember[i] ) // element of array remember is not an empty string
+		{
+			var x = getObj( remember[i] );
+			x.visibility = 'hidden';
+		}
+		remember[i] = null; // forget this level
+	}
+}
+
+/*
+	return style of item we are looking for, to make it visible or hidden
+*/
+function getObj(name)
+{
+	if( document.getElementById )
+		return document.getElementById( name ).style;
+
+	if( document.all )
+		return document.all[name].style;
+
+	if( document.layers )
+		return document.layers[name];
+	
+	return false;
+}

766aefdd9ba43bb0e76711d24dbb307dc865d2b9
Léna Yumie Trullier <lena.trullier@icloud.com>
2024-11-07 14:54:21 +0100

    wild and free

--- script1.js
+++ script1.js
@@ -1,11 +1,5 @@
 var myVideo1 = document.getElementById("video1"); 
 
- /* function playPause1() { 
-    if (myVideo1.paused) 
-      myVideo1.play(); 
-    else 
-      myVideo1.pause(); 
-  } */
   function makeBig1() { 
     myVideo1.width = 1160;
     myVideo1.height = 651; 
@@ -17,12 +11,6 @@ var myVideo1 = document.getElementById("video1");
   
 var myVideo2 = document.getElementById("video2"); 
 
- /* function playPause2() { 
-    if (myVideo2.paused) 
-      myVideo2.play(); 
-    else 
-      myVideo2.pause(); 
-  } */
   function makeBig2() { 
     myVideo2.width = 1160; 
     myVideo2.height = 651; 
@@ -34,12 +22,6 @@ var myVideo2 = document.getElementById("video2");
 
 var myVideo3 = document.getElementById("video3"); 
 
- /* function playPause3() { 
-    if (myVideo3.paused) 
-      myVideo3.play(); 
-    else 
-      myVideo3.pause(); 
-  } */
   function makeBig3() { 
     myVideo3.width = 1160; 
     myVideo3.height = 651; 
@@ -49,98 +31,68 @@ var myVideo3 = document.getElementById("video3");
     myVideo3.height = 300; 
   }
 
- var myVideo4 = document.getElementById("video4"); 
+var myVideo4 = document.getElementById("video4"); 
 
-  /* function playPause4() { 
-     if (myVideo4.paused) 
-       myVideo4.play(); 
-     else 
-       myVideo4.pause(); 
-   } */
-   function makeBig4() { 
-     myVideo4.width = 1160; 
-     myVideo4.height = 651; 
-   } 
-   function makeSmall3() {
-     myVideo4.width = 530; 
-     myVideo4.height = 300; 
-   }
+  function makeBig4() { 
+    myVideo4.width = 1160; 
+    myVideo4.height = 651; 
+  } 
+  function makeSmall3() {
+    myVideo4.width = 530; 
+    myVideo4.height = 300; 
+  }
   
 var myVideo5 = document.getElementById("video5"); 
 
-   /* function playPause5() { 
-      if (myVideo5.paused) 
-        myVideo5.play(); 
-      else 
-        myVideo5.pause(); 
-    } */
-    function makeBig5() { 
-      myVideo5.width = 1097; 
-      myVideo5.height = 633; 
-    } 
-    function makeSmall5() {
-      myVideo5.width = 500; 
-      myVideo5.height = 300; 
-    }
+  function makeBig5() { 
+    myVideo5.width = 1097; 
+    myVideo5.height = 633; 
+  } 
+  function makeSmall5() {
+    myVideo5.width = 500; 
+    myVideo5.height = 300; 
+  }
 
 var myVideo6 = document.getElementById("video6"); 
 
-   /* function playPause6() { 
-      if (myVideo6.paused) 
-        myVideo6.play(); 
-      else 
-        myVideo6.pause(); 
-    } */
-    function makeBig6() { 
-      myVideo6.width = 1010; 
-      myVideo6.height = 633; 
-    } 
-    function makeSmall6() {
-      myVideo6.width = 480; 
-      myVideo6.height = 300; 
-    }
+  function makeBig6() { 
+    myVideo6.width = 1010; 
+    myVideo6.height = 633; 
+  } 
+  function makeSmall6() {
+    myVideo6.width = 480; 
+    myVideo6.height = 300; 
+  }
 
-    var myVideo7 = document.getElementById("video7"); 
+var myVideo7 = document.getElementById("video7"); 
 
-    /* function playPause7() { 
-       if (myVideo7.paused) 
-         myVideo7.play(); 
-       else 
-         myVideo7.pause(); 
-     } */
-     function makeBig7() { 
-       myVideo7.width = 867; 
-       myVideo7.height = 633; 
-     } 
-     function makeSmall7() {
-       myVideo7.width = 410; 
-       myVideo7.height = 300; 
-     }
+  function makeBig7() { 
+    myVideo7.width = 867; 
+    myVideo7.height = 633; 
+  } 
+  function makeSmall7() {
+    myVideo7.width = 410; 
+    myVideo7.height = 300; 
+  }
 
-     var myVideo8 = document.getElementById("video8"); 
+var myVideo8 = document.getElementById("video8"); 
 
-     /* function playPause8() { 
-        if (myVideo8.paused) 
-          myVideo8.play(); 
-        else 
-          myVideo8.pause(); 
-      } */
-      function makeBig8() { 
-        myVideo8.width = 1160
-        myVideo8.height = 827; 
-      } 
-      function makeSmall8() {
-        myVideo8.width = 580;
-        myVideo8.height = 417; 
-      }
+  function makeBig8() { 
+    myVideo8.width = 1160
+    myVideo8.height = 827; 
+  } 
+  function makeSmall8() {
+     myVideo8.width = 580;
+    myVideo8.height = 417; 
+  }
 
-      var myVideo9 = document.getElementById("video9"); 
+var myVideo9 = document.getElementById("video9"); 
 
-       function makeBig9() { 
-         myVideo9.width = 1160
-         myVideo9.height = 827; 
-       } 
-       function makeSmall9() {
-         myVideo9.width = 580;
-         myVideo9.height = 817; 
-       }
+  function makeBig9() { 
+    myVideo9.width = 1160
+    myVideo9.height = 827; 
+  } 
+  function makeSmall9() {
+    myVideo9.width = 580;
+    myVideo9.height = 817; 
+  }
